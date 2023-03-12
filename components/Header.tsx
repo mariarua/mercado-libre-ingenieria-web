e7e6f7801f56c1d61ba7c15352fc3faf58c5791f
mariarua <maria.rua1@udea.edu.co>
2023-03-12 15:17:22 -0500

    Refactor header

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,43 +1,23 @@
 import React from "react";
 import Image from "next/image";
 import logo from "../public/logo-header.svg";
+import SearchBar from "./SearchBar";
+import ToolBar from "./ToolBar";
 
 function Header() {
   return (
-    <div className="bg-primary">
-      <div className="flex ">
-        <Image src={logo} alt="Logo de Mercado Libre" width={120} height={35} />
-        <h2>Buscar productos, marcas y más...</h2>
+    <div className="bg-primary w-screen">
+      <div className="flex py-2">
+        <Image
+          className="ml-40 mr-8"
+          src={logo}
+          alt="Logo de Mercado Libre"
+          width={134}
+          height={34}
+        />
+        <SearchBar />
       </div>
-      <nav className="">
-        <ul>
-          <li>
-            <a href="#">Ingresa tu domicilio</a>
-          </li>
-          <li>
-            <a href="#">Categorías -buscar flecha a abajo</a>
-          </li>
-          <li>
-            <a href="#">Ofertas</a>
-          </li>
-          <li>
-            <a href="#">Historial</a>
-          </li>
-          <li>
-            <a href="#">Supermercado</a>
-          </li>
-          <li>
-            <a href="#">Moda</a>
-          </li>
-          <li>
-            <a href="#">Vender</a>
-          </li>
-          <li>
-            <a href="#">Ayuda / PQR</a>
-          </li>
-        </ul>
-      </nav>
-      <div>Crea tu cuenta Ingresa Mis compras</div>
+      <ToolBar />
     </div>
   );
 }
