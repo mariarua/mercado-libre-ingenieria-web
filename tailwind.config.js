c62c36929ec663ab17fc8d37c6fac1bc483d9cd2
mariarua <maria.rua1@udea.edu.co>
2023-03-15 19:06:25 -0500

    Create custom width and backgroundImage

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,6 +10,9 @@ module.exports = {
   ],
   theme: {
     extend: {
+      width: {
+        desktop: "1200px",
+      },
       colors: {
         primary: "#FFF158",
         homepage: "#EBEBEB",
@@ -20,6 +23,9 @@ module.exports = {
         narrow: "0 1px 2px 0 rgb(0 0 0 / 20%)",
         extranarrow: "0px 1px 1px rgba(0, 0, 0, 0.1)",
         m: "0 2px 15px 0 rgb(0 0 0 / 10%)",
+      },
+      backgroundImage: {
+        susbcribe: "linear-gradient(90deg,#a90f90 55%,#0c1a51)",
       },
     },
   },
